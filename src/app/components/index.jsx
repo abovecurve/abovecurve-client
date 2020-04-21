2af813a2114a4d7fb1951e6d2a245447f7c50be5
plaird730 <plaird730@gmail.com>
2020-04-21 09:23:50 -0400

    Forcing a push to an existing pull request to ensure it triggers CI/CD

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -9,9 +9,9 @@ class App extends Component {
   };
   componentDidMount = () => {
     // custom axios uses env specific base path
-    // LOCAL_API="http://localhost:4000/ping"
-    // REACT_APP_API_URL="http://api.abovecurve.com/ping" (production)
-    // REACT_APP_API_URL="http://api.abovecurve.dev/ping" (development)
+    // LOCAL_API="http://localhost:4000"
+    // REACT_APP_API_URL="http://api.abovecurve.com" (production)
+    // REACT_APP_API_URL="http://api.abovecurve.dev" (development)
     axios.get(`/ping`).then((res) => {
       const data = res.data;
       this.setState({ data });
@@ -23,6 +23,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Welcome to Above Curve!</p>
+          {/* Below line proves that the client talks to the api */}
+          {/* Should output: Hello from the Above Curve API! */}
           {this.state.data.message}
         </header>
       </div>
