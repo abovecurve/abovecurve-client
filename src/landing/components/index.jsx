17fdbe78669f6e73bccbaf6d881d31a75b3c0939
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-04-22 22:14:27 -1000

    adds hover to menu button and us map to landing page

--- src/landing/components/index.jsx
+++ src/landing/components/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./landing.module.scss";
 
+import USMap from "../assets/Blank_US_Map.png";
+
 const Landing = (props) => {
-  return <div className={styles.Landing}>Landing Component</div>;
+  return (
+    <div className={styles.Landing}>
+      <img src={USMap} alt="" className={styles.US_map} />
+    </div>
+  );
 };
 
 export default Landing;
--- src/navbar/components/index.jsx
+++ src/navbar/components/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./navbar.module.scss";
+import logo_512 from "../assets/ac_logo_512.png";
 
 const Navbar = (props) => {
-  return <div className={styles.Navbar}>Navbar</div>;
+  return (
+    <div className={styles.Navbar}>
+      <img src={logo_512} alt="Above Curve" />
+      <div className={styles.menuButton}>
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
