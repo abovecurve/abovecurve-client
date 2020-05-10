6e543bf071a9f55e79bf30e1b0618ee4b1d59176
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-05-09 15:21:23 -1000

    saves map index file

--- src/map/index.jsx
+++ src/map/index.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./map.module.scss";
 import { ReactComponent as USMapSVG } from "./assets/US_map-01.svg";
 import MapMenu from "../mapMenu";
 
 const Map = (props) => {
+  useEffect(() => {
+    let map = document.getElementsByTagName("svg");
+    let paths = map[0].querySelectorAll("path");
+
+    paths.forEach((path) => {
+      let bboxRec = path.getBBox();
+      console.log(bboxRec);
+    });
+  });
+
   return (
     <div className={styles.Map}>
       <USMapSVG className={styles.US_map} data-testid="USMapSVG" />
