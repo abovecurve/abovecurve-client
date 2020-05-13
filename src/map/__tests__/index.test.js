116953cb7310c5c38ca788d657c08b4db1d125e1
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-05-12 17:39:25 -1000

    adds dots to every state based on a variable that we can change later, ran into testing issues seeing svgs

--- src/map/__tests__/index.test.js
+++ src/map/__tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Map from "../index";
-import { render, cleanup, fireEvent } from "../../utils/test-utils";
+import { render, cleanup, fireEvent, screen } from "../../utils/test-utils";
 
 const setUp = (props = {}) => {
   const component = render(<Map {...props} />);
@@ -19,4 +19,6 @@ describe("Map Component", () => {
     const map = component.getAllByTestId("USMapSVG");
     expect(map.length).toBe(1);
   });
+
+  //not really sure how to test that all states are getting an svg dot, created in the useEffect method, from what I read online, it looks like JSDOM (via jest) doesn't support createSVGPoint...
 });
--- src/map/index.jsx
+++ src/map/index.jsx
@@ -7,10 +7,34 @@ const Map = (props) => {
     let map = document.getElementsByTagName("svg");
     let paths = map[0].querySelectorAll("path");
 
-    paths.forEach((path) => {
-      let bboxRec = path.getBBox();
-      console.log(bboxRec);
-    });
+    for (let key of paths.keys()) {
+      let bbox = paths[key].getBBox();
+
+      let center = {
+        x: bbox.x + bbox.width / 2,
+        y: bbox.y + bbox.height / 2,
+      };
+
+      // changes dynamically based on data provided by dataset
+      let size = 10;
+
+      let svgns = "http://www.w3.org/2000/svg";
+      let dot = document.createElementNS(svgns, "circle");
+      dot.setAttribute("cx", center.x);
+      dot.setAttribute("cy", center.y);
+      dot.setAttribute("fill", "#ff0000");
+      dot.setAttribute("fill-opacity", ".3");
+      dot.setAttribute("stroke", "#ff0000");
+      dot.setAttribute("stroke-width", "1");
+      dot.setAttribute("r", size);
+      dot.setAttribute("data-testid", `circle`);
+
+      let title = document.createElement("title");
+      title.innerHTML = `circle${key}`;
+
+      dot.appendChild(title);
+      map[0].appendChild(dot);
+    }
   });
 
   return (
