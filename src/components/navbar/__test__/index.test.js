0b144c8bb09bc485bb45ca9cb7bbf9ca7a6c6ffa
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-05-23 17:38:06 -1000

    adds change to menu button on click to display an x, adds tests to check menu button change and display of MapMenu on menu button click

--- src/components/navbar/__test__/index.test.js
+++ src/components/navbar/__test__/index.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "../index";
-import { render, cleanup, fireEvent } from "../../../utils/test-utils";
+import MapMenu from "../../mapMenu";
+import { render, cleanup, fireEvent, screen } from "../../../utils/test-utils";
+import { initialState } from "../../../reducers";
 
 const setUp = (props = {}) => {
-  const component = render(<Navbar {...props} />);
+  const component = render(<Navbar {...props} />, {
+    initialState: { menuOpen: false },
+  });
   return component;
 };
 
@@ -26,8 +30,27 @@ describe("Navbar Component", () => {
   });
 
   describe("Menu button", () => {
-    it("should change the props.menuOpen value when clicked", () => {
+    it("should display the MapMenu if clicked", () => {
+      const uniqueComponent = render(
+        <div>
+          <Navbar />
+          <MapMenu />
+        </div>
+      );
+
+      const menuBtn = uniqueComponent.getAllByLabelText("Menu")[0];
+
+      fireEvent.click(menuBtn);
+
+      expect(screen.getAllByTestId("MapMenu").length).toBe(1);
+    });
+
+    it("should display an x if the menu is open", () => {
       const menuBtn = component.getAllByLabelText("Menu")[0];
+
+      fireEvent.click(menuBtn);
+
+      expect(component.getAllByTestId("xBtn").length).toBe(1);
     });
   });
 });
