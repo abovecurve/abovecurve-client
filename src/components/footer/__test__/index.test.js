0923c32b4bb51918f9162c1ca426895ad2a3d19f
lalliere <emilylallier@yahoo.com>
2020-06-06 15:17:30 -0400

    attempted footer test file

---
+++ src/components/footer/__test__/index.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "../index";
+import { render, cleanup, fireEvent } from "../../../utils/test-utils";
+
+const setUp = (props = {}) => {
+  const component = render(<Footer {...props} />);
+  return component;
+};
+
+describe("Footer Component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  afterEach(cleanup);
+
+  it("should render the ac-logo without error", () => {
+    const logo = component.getAllByTestId("ac-logo");
+    expect(logo.length).toBe(1);
+  });
+});
