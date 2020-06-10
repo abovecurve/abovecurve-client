7b363bf6cbbc92970e9c584411c4330de5ab196d
Danielle Daley <danielledaley2196@gmail.com>
2020-06-09 21:17:13 -0400

    fixed style of landing componant

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,43 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import styles from "./landing.module.scss";
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      //<div className="container valign-wrapper">
+      <Grid container direction="row" justify="center" alignItems="center">
         <div className="row">
           <div className="col s12 center-align">
             <h1>Welcome to Above Curve</h1>
-            <p>Please either log in or register an account</p>
+            <p>Please either log in or register an account.</p>
             <br />
             <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
+              <Button>
+                <Link to="/register" className={styles.Button}>
+                  Register
+                </Link>
+              </Button>
             </div>
             <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
-              </Link>
+              <Button>
+                <Link to="/login" className={styles.Button}>
+                  Log In
+                </Link>
+              </Button>
             </div>
           </div>
         </div>
-      </div>
+      </Grid>
+      //</div>
     );
   }
 }
