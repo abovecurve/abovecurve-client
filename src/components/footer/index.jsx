9c6c4ff322d7051faec08011932450c51287b57d
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-06-30 11:17:58 -1000

    changes footer from material UI to basic footer element because of testing issues, will need to be changed back in the future

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "./assets/ac_logo_24.png";
-import BottomNavigation from '@material-ui/core/BottomNavigation';
+
+// <footer/> was originally BottomNavigation component, was causing a test error so removed until BottomNavigationAction can be implemented
+// import BottomNavigation from "@material-ui/core/BottomNavigation";
 import styles from "./footer.module.scss";
 
-
 export default function Footer() {
-
-
   return (
-    <BottomNavigation className={styles.Footer} showLabels>
+    <footer className={styles.Footer}>
       <img src={logo} alt="Above Curve" data-testid="ac-footer-logo"></img>
-    </BottomNavigation>
+    </footer>
   );
 }
